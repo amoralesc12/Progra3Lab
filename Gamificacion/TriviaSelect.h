d8b789d9f5866d8aacf94cf19d143eb95de9284a
amoralesc12 <alejandramorales.2001@unitec.edu>
2021-11-05 18:38:16 -0600

    mi parte terminada para mañana
    agregue algunas cosas y termine lo que me faltaba

--- Gamificacion/TriviaSelect.h
+++ Gamificacion/TriviaSelect.h
@@ -10,11 +10,16 @@ class TriviaSelect
 public:
 	void loadSprites_TS();
 	void loadScreen_TS();
+	void loadQuestionBoxScreen_TS();
+	void loadQuestionBoxSprites_TS();
 
 private:
 
 	Sprite bgN1, qb1, qb2,qb3,qb4,qb5,qb6,qb7,qb8;
 	Texture bg, b1,b2,b3,b4,b5,b6,b7,b8;
+
+	Sprite pregunEj, respEj;
+	Texture pEj,rEj;
 
 	Event evento;
 
--- Gamificacion/elegirBando.cpp
+++ Gamificacion/elegirBando.cpp
@@ -12,11 +12,22 @@ using namespace sf;
 void elegirBando::addingSprites_B() {
 
 	if (!bgB.loadFromFile("Resources/bgElegirBando.jpg")) { cout << "NO ESTA"; }
-
+	if (!g1.loadFromFile("Resources/textoR.png")) { cout << "NO SE ENCUENTRA"; }
+	if (!g2.loadFromFile("Resources/textoE.png")) { cout << "NO SE ENCUENTRA"; }
+	if (!mensjB.loadFromFile("Resources/mBando.png")) { cout << "NO SE ENCUENTRA"; }
 
 	bgBando.setTexture(bgB);
 	bgBando.setPosition(0, 0);
 
+	text1.setTexture(g1);
+	text1.setPosition(150, 350);
+
+
+	text2.setTexture(g2);
+	text2.setPosition(650, 350);
+
+	mensj.setTexture(mensjB);
+	mensj.setPosition(10, 50);
 }
 
 void elegirBando::loadScreen_B()
@@ -35,11 +46,18 @@ void elegirBando::loadScreen_B()
 				Vector2i mousePos = Mouse::getPosition(window);
 				Vector2f mousePosF(static_cast<float>(mousePos.x), static_cast<float>(mousePos.y));
 
-				if (this->bgBando.getGlobalBounds().contains(mousePosF))
+				if (this->text1.getGlobalBounds().contains(mousePosF))
 				{
 					TriviaSelect choose;
 					window.close();
 					choose.loadScreen_TS();
+				}
+				else if (this->text2.getGlobalBounds().contains(mousePosF))
+				{
+					TriviaSelect choose;
+					window.close();
+					choose.loadScreen_TS();
+
 				}
 
 			}
@@ -48,7 +66,9 @@ void elegirBando::loadScreen_B()
 
 		window.clear(Color::Black);
 		window.draw(this->bgBando);
-
+		window.draw(this->text1);
+		window.draw(this->text2);
+		window.draw(this->mensj);
 		window.display();
 	}
 
