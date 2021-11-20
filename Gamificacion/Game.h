7122fbe3ba2405dd81378b95436af48d4c919368
jjtorre <jesustorres@unitec.edu>
2021-11-20 07:20:26 -0600

    Agregando preguntas a Globos

---
+++ Gamificacion/Game.h
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef GAME_H
+#define GAME_H
+//COMMIT
+#include "SFML/Graphics.hpp"
+#include <array>
+
+class Game
+{
+public:
+	Game();
+	void run();
+private:
+	void processEvents();
+	//void update();
+	void update(sf::Time);
+	void render();
+	void handlePlayerInput(sf::Keyboard::Key, bool);
+	bool mIsMovingUp, mIsMovingDown, mIsMovingLeft, mIsMovingRight;
+	float PlayerSpeed;
+	void load_graphs();
+
+
+
+private:
+	sf::RenderWindow mWindow;
+	//sf::CircleShape mPlayer;
+	sf::Time TimePerFrame;
+	std::array< sf::Texture, 4>tex_globos;
+	std::array< sf::Sprite, 4>spr_globos;
+	sf::Texture tex_Ground;
+	sf::Texture tex_Pointer;
+	sf::Sprite spr_Pointer;
+	sf::Sprite spr_Ground;
+	sf::Text texto;
+	sf::Font fuente;
+};
+
+#endif // GAME_H
+
+
---
+++ Gamificacion/Question.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef QUESTION_H
+#define QUESTION_H
+//COMMIT
+#include<string>
+#include<array>
+
+using namespace std;
+class Question
+{
+public:
+	Question();
+	int getQuestion();
+	int getResp();
+	array<array<string, 7>, 10 >q;
+
+
+private:
+	//array de 5 columnas y 20 filas
+	//Columna 1 guarda la Pregunta, de la 2 a la 5 las opciones, la columna 6 el numero de respuesta correcta y la 7 el tipo de pregunta(0-> Arte, 2->Politica ...)
+
+	int temF;
+	int temC;
+
+};
+#endif // !QUESTION_H
+
+
